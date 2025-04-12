f461e21fd1135c8591e7baeeca8d42c17ad5713c
superres <130553076+superres@users.noreply.github.com>
2025-04-12 13:34:03 +0800

    Add files via upload

---
+++ arrange_files.m
@@ -0,0 +1,28 @@
+clc;clear;close all; warning off;
+
+fs0='raw_'; %fs0='E:\Papers\my own\iOTF\rebuttal\simulated\raw_';
+%%stack files of 100 Mb were separated into 5 stacks 20 Mb to avoid 25Mb
+%%upload limitation of GitHub
+for i=1:9
+    fs1=sprintf('%d.tif',i);
+    fs1=[fs0,fs1];
+    a=readTIF(fs1);
+    for j=1:5
+        a1=a(:,:,j*20-19:j*20);
+        fs2=sprintf('%d.%d.tif',i,j);
+        fs2=[fs0,fs2];
+        writeTIF(fs2,a1);
+    end
+end
+
+%%5 separated stacks of 20 Mb were added up as one stach (100 Mb)
+for i=1:9
+    for j=1:5
+        fs2=sprintf('%d.%d.tif',i,j);
+        fs2=[fs0,fs2];
+        a(:,:,j*20-19:j*20)=readTIF(fs2);
+    end
+    fs1=sprintf('%d.tif',i);
+    fs1=[fs0,fs1];
+    writeTIF(fs1,a);
+end
